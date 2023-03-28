5b293e975ed3f4a22cebb93f140b10f75931be06
Eric-Canas <elcorreodeharu@gmail.com>
2023-03-28 19:01:39 +0200

    logo

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='usb-monitor',
-    version='1.0',
+    version='1.1',
     author='Eric-Canas',
     author_email='eric@ericcanas.com',
     url='https://github.com/Eric-Canas/USBMonitor',
