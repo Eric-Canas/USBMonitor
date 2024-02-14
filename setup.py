f85204894224197447f2569ec956e509bf30a530
Eric-Canas <elcorreodeharu@gmail.com>
2024-02-14 17:37:00 +0100

    1.17 update for PyPi

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='usb-monitor',
-    version='1.16',
+    version='1.17',
     author='Eric-Canas',
     author_email='eric@ericcanas.com',
     url='https://github.com/Eric-Canas/USBMonitor',
