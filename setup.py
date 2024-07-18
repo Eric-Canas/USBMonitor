98e94f8c4847dd9614405325ec2ad04977cf539b
Eric Cañas <elcorreodeharu@gmail.com>
2024-07-18 15:48:45 +0200

    Bump to 1.21

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='usb-monitor',
-    version='1.20',
+    version='1.21',
     author='Eric-Canas',
     author_email='eric@ericcanas.com',
     url='https://github.com/Eric-Canas/USBMonitor',
