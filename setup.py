b25bea1162373efb5eb3ea25a9951c99aa20cd44
Trisha Clemente <patricianvclemente@gmail.com>
2024-11-05 14:47:41 +1100

    Updated name in setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='usb-monitor',
+    name='USBMonitor',
     version='1.21',
     author='Eric-Canas',
     author_email='eric@ericcanas.com',
